099e101a1e1bf03162a99d50f4e01f86c2ae5a2f
imam9 <imamzunaidi4@gmail.com>
2021-07-30 00:33:14 +0700

    configurasi payment gateway awal

--- application/views/konsumen/keranjang.php
+++ application/views/konsumen/keranjang.php
@@ -33,12 +33,70 @@
 	</table>
 
 	<div align="right">
+		<form id="payment-form" method="post" action="<?=site_url()?>payment/snap/finish">
+			<input type="hidden" name="result_type" id="result-type" value="">
+			<input type="hidden" name="result_data" id="result-data" value="">
+		</form>
 		<a href="<?php echo base_url('konsumen/dashboard/hapus_keranjang') ?>"><div class="btn btn-sm btn-danger">
 			Hapus Keranjang</div></a>
 		<a href="<?php echo base_url('konsumen/dashboard/index') ?>"><div class="btn btn-sm btn-primary">
 			Lanjutkan Belanja</div></a>
-		<a href="<?php echo base_url('konsumen/dashboard/checkout') ?>"><div class="btn btn-sm btn-success">
-			Checkout</div></a>
+		<button id="pay-button" class="btn btn-sm btn-success" >
+		Checkout</a></button>
 	</div>
-
 </div>
+    
+    
+    <script type="text/javascript">
+  
+    $('#pay-button').click(function (event) {
+      event.preventDefault();
+      $(this).attr("disabled", "disabled");
+    
+    $.ajax({
+      url: '<?=site_url()?>payment/snap/token',
+      cache: false,
+
+      success: function(data) {
+        //location = data;
+
+        console.log('token = '+data);
+        
+        var resultType = document.getElementById('result-type');
+        var resultData = document.getElementById('result-data');
+
+        function changeResult(type,data){
+          $("#result-type").val(type);
+          $("#result-data").val(JSON.stringify(data));
+          //resultType.innerHTML = type;
+          //resultData.innerHTML = JSON.stringify(data);
+        }
+
+        snap.pay(data, {
+          
+          onSuccess: function(result){
+            changeResult('success', result);
+            console.log(result.status_message);
+            console.log(result);
+            $("#payment-form").submit();
+          },
+          onPending: function(result){
+            changeResult('pending', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          },
+          onError: function(result){
+            changeResult('error', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          }
+        });
+      }
+    });
+  });
+
+  </script>
+
+
+</body>
+</html>
