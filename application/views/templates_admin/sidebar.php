c1792db845225eb24f602efea782486b45ac5c1e
Taufik Windows <taufik.amary@gmail.com>
2021-07-30 12:49:48 +0700

    datatable pada admin

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -79,37 +79,19 @@
                         <i class="fa fa-bars"></i>
                     </button>
 
-                    <!-- Topbar Search -->
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
                         <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
                             </a>
                             <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                                         <div class="input-group-append">
                                             <button class="btn btn-primary" type="button">
                                                 <i class="fas fa-search fa-sm"></i>
@@ -121,21 +103,23 @@
                         </li>
 
                         <div class="navbar">
-                            
+
                             <div class="topbar-divider d-none d-sm-block"></div>
-                                
-                                <ul class="nav navbar-nav navbar-right">
-                                    
-                                    <?php if($this->session->userdata('username')) { ?>
 
-                                        <li><div> Selamat Datang <?php echo $this->session->userdata('username') ?>
-                                              </div></li>
-                                        <li class="ml-2"><?php echo anchor('auth/logout', 'Logout'); ?></li>
-                                    <?php }else{ ?>
-                                        <li><?php echo anchor('auth/login', 'Login'); ?></li>
-                                    <?php } ?>
+                            <ul class="nav navbar-nav navbar-right">
 
-                                </ul>
+                                <?php if ($this->session->userdata('username')) { ?>
+
+                                    <li>
+                                        <div> Selamat Datang <?php echo $this->session->userdata('username') ?>
+                                        </div>
+                                    </li>
+                                    <li class="ml-2"><?php echo anchor('auth/logout', 'Logout'); ?></li>
+                                <?php } else { ?>
+                                    <li><?php echo anchor('auth/login', 'Login'); ?></li>
+                                <?php } ?>
+
+                            </ul>
 
                         </div>
                     </ul>
